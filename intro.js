402dcc3e8dcb828ebc839098f35d9b16e635b9eb
Abba Aminu Ibrahim (Abberhjanxaky) <abbaaminuonline@gmail.com>
2026-06-13 22:53:23 +0100

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,17 @@
+<script>
+let name = prompt("Abba Aminu Ibrahim :");
+let age = prompt("19:");
+let favouriteLanguage = prompt("python:");
+
+
+alert(
+  "Name: " + name +
+  "\nAge: " + age +
+  "\nFavourite Programming Language: " + favouriteLanguage
+);
+
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Programming Language:", favouriteLanguage);
+</script>
